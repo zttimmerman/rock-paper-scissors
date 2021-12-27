492f7f6867f75bd80f9711576be5ee8e18ee763b
Zach Timmerman <zachary.timmerman@gmail.com>
2021-12-26 19:40:46 -0500

    Rock, Paper, Scissors v1.2 Add basic styling and icons, header and footer.

--- script.js
+++ script.js
@@ -10,39 +10,38 @@ function playRound(e) {
     let computerSelection = computerPlay();
     let playerSelection = e.target.id;
     let results = document.querySelector("#results");
-
     if (playerSelection === "rock" && computerSelection == "scissors") {
         playerScore++;
-        results.textContent = "You Win! Rock beats Scissors";
+        results.textContent = "Round won! Rock beats Scissors";
         displayScore();
     } else if (playerSelection === "rock" && computerSelection == "paper") {
         computerScore++;
-        results.textContent = "You Lose! Paper beats Rock";
+        results.textContent = "Round lost! Paper beats Rock";
         displayScore();
     } else if (playerSelection === "rock" && computerSelection == "rock") {
         results.textContent = "Tie!";
         displayScore();
     } else if (playerSelection === "paper" && computerSelection == "scissors") {
         computerScore++;
-        results.textContent = "You Lose! Scissors beats Paper"
+        results.textContent = "Round lost! Scissors beats Paper"
         displayScore();
     } else if (playerSelection === "paper" && computerSelection == "paper") {
         results.textContent = "Tie!";
         displayScore();
     } else if (playerSelection === "paper" && computerSelection == "rock") {
         playerScore++;
-        results.textContent = "You Win! Paper beats Rock"
+        results.textContent = "Round won! Paper beats Rock"
         displayScore();
     } else if (playerSelection === "scissors" && computerSelection == "scissors") {
         results.textContent = "Tie!";
         displayScore();
     } else if (playerSelection === "scissors" && computerSelection == "paper") {
         playerScore++;
-        results.textContent = "You You Win! Scissors beats Paper";
+        results.textContent = "Round won! Scissors beats Paper";
         displayScore();
     } else if (playerSelection === "scissors" && computerSelection == "rock") {
         computerScore++;
-        results.textContent = "You Lose! Rock beats Scissors";
+        results.textContent = "Round lost! Rock beats Scissors";
         displayScore();
     } else {
         results.textContent = "ERROR"
@@ -54,12 +53,14 @@ function playRound(e) {
 function checkWinner() {
     const para = document.createElement("p");
     if (playerScore === 5) {
-        para.textContent = "Congratulations, you won!";
+        para.textContent = "Congratulations, you won the game!";
+        para.style.cssText = "color: green; font-weight: 700; font-size: 1.1em;"
         results.appendChild(para);
         playerScore = 0;
         computerScore = 0;
     } else if (computerScore === 5) {
-        para.textContent = "Sorry, you lost!";
+        para.textContent = "Sorry, you lost the game!";
+        para.style.cssText = "color: red; font-weight: 700; font-size: 1.1rem;"
         results.appendChild(para);
         playerScore = 0;
         computerScore = 0;
@@ -71,6 +72,7 @@ function checkWinner() {
 function displayScore() {
     const score = document.createElement("p");
     score.textContent = `Your Score: ${playerScore} Computer Score: ${computerScore}`;
+    score.style.cssText = "font-weight: 700; padding: 1.1rem"
     results.appendChild(score);
 }
 
@@ -81,18 +83,3 @@ const btnScissors = document.querySelector("#scissors")
 btnRock.addEventListener("click", playRound);
 btnPaper.addEventListener("click", playRound);
 btnScissors.addEventListener("click", playRound);
-
-/* function game() {
-    let playerChoice;
-    for (i = 0; i < 5; i++) {
-        playerChoice = prompt("Rock, Paper, or Scissors?");
-        playRound(playerChoice, computerPlay());
-    }
-    if (playerScore > computerScore) {
-        console.log("Congratulations! You won!")
-    } else if (playerScore < computerScore) {
-        console.log("Sorry! You lost!")
-    } else if (playerScore === computerScore) {
-        console.log("Tie!")
-    }
-} */
